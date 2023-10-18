ae9bb2a75789bca3607f5bebcc61b5c2c24173e9
baden-dev <baden.dev@gmail.com>
2023-10-18 23:49:47 +0200

    initial commit

---
+++ src/main/java/com/baden/zacodinginstitutionsapi/ZaCodingInstitutionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.baden.zacodinginstitutionsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaCodingInstitutionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaCodingInstitutionsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baden/zacodinginstitutionsapi/ZaCodingInstitutionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baden.zacodinginstitutionsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaCodingInstitutionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
