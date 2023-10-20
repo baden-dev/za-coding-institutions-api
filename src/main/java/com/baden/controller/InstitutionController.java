dc3727cf5612092d8f10f6dee307f1464746e06f
baden-dev <baden.dev@gmail.com>
2023-10-20 13:53:08 +0200

    added institution controller responsible for handling http requests, routing them to the appropriate endpoints based on the url and http method.

---
+++ src/main/java/com/baden/controller/InstitutionController.java
@@ -0,0 +1,53 @@
+package com.baden.controller;
+
+import com.baden.customResponse.SuccessResponse;
+import com.baden.entity.InstitutionEntity;
+import com.baden.service.InstitutionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class InstitutionController {
+
+    private final InstitutionService institutionService;
+
+    public InstitutionController(InstitutionService institutionService) {
+        this.institutionService = institutionService;
+    }
+
+    // Create a new institution
+    @PostMapping("/institution")
+    public ResponseEntity<InstitutionEntity> save(@RequestBody InstitutionEntity institutionEntity) {
+        return ResponseEntity.ok().body(institutionService.saveInstitution(institutionEntity));
+    }
+
+    // Get all institutions
+    @GetMapping("/institutions")
+    public ResponseEntity<List<InstitutionEntity>> getAll() {
+        return ResponseEntity.ok().body(institutionService.getAllInstitutions());
+    }
+
+    // Get institution by id
+    @GetMapping("/institution/{institution_id}")
+    public ResponseEntity<InstitutionEntity> getById(@PathVariable Integer institution_id) {
+        return ResponseEntity.ok().body(institutionService.getInstitutionById(institution_id));
+    }
+
+    // Update institution details by idgi
+    @PutMapping("/institution/{institution_id}")
+    public ResponseEntity<InstitutionEntity> update(@PathVariable Integer institution_id, @RequestBody InstitutionEntity institutionEntity) {
+        return ResponseEntity.ok().body(institutionService.updateInstitution(institution_id, institutionEntity));
+
+    }
+
+    //Delete institution by id
+    @DeleteMapping("/institution/{institution_id}")
+    public ResponseEntity<SuccessResponse> delete(@PathVariable Integer institution_id) {
+        return ResponseEntity.ok().body(institutionService.deleteInstitution(institution_id));
+    }
+
+}
