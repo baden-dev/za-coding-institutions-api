20bd09e58770564fe3ecef80253df85018b73741
baden-dev <baden.dev@gmail.com>
2023-10-30 00:02:06 +0200

    added target folder into repo because Dockerfile require jar in folder

---
+++ target/classes/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO institution (institution_name, institution_description, institution_email, institution_phone, institution_website)
+VALUES
+    ('WeThinkCode_', 'Wethinkcode_ offers a two year, tuition free, NQF level 5 qualification in systems development, with a guaranteed path to employment.',
+     'info@wethinkcode.co.za', ' 021 202 5424', 'www.wethinkcode.co.za'),
+    ('Umuzi', 'Umuzi trains top talent for digital roles at leading employers · We offer fully remote, personalised full-time and part-time training.',
+     'info@umuzi.org', null, 'www.umuzi.org'),
+    ('CodeSpace Academy', 'CodeSpace equips you with one of the most sought-after skill sets in the world. Our curated learning pathway is the most effective way to become a developer.',
+     'learn@codespace.co.za', '+27 21 685 0991', 'www.codespace.co.za'),
+    ('CodeX', 'codeX aims to solve two of Africa’s major challenges: youth unemployment and the programming skills gap. We find, train, and place young developer talent in great entry-level jobs and ensure they have the solid foundation they need to build long-term digital careers.',
+     'info@projectcodeX.co', '021 054 0125', 'www.projectcodex.co'),
+    ('iXperience', 'iXperience started in 2013 as a study abroad coding bootcamp that would bridge the gap between academia and industry. We go to great lengths to get to know our students, teachers, and partner companies, which we believe has been instrumental to our success.',
+     'hello@ixperience.co', null, 'www.ixperience.co'),
+    ('School of IT', 'School of IT is a leading tech education company, created and started by an IT enthusiast in 2015. 2x Internationally Award-winning courses (Best Online Coding Bootcamp winner 2020 & Best Online Coding Bootcamp winner 2021). We have the vision to be the leaders in the IT training Industry, in South Africa and Internationally.',
+     'info@schoolofit.co.za', '+2782 696 7749', 'www.schoolofit.co.za');
